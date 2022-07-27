3c1dd7ebdca526e30670b2e152f6074d9ae05392
Giovanni Marino <GiovaMarino1@gmail.com>
2022-07-26 23:56:54 -0400

    styled basic begin page format

--- src/Begin.js
+++ src/Begin.js
@@ -1,7 +1,16 @@
 const Begin = ({ buttonRemover }) => {
   return (
     <div className="Begin">
-      <button onClick={buttonRemover}>Begin</button>
+      <div className="beginLeft">
+        <p>
+          Answer this quiz to find out where you belong on a Pokémon moral
+          alignment chart
+        </p>
+        <button onClick={buttonRemover}>Begin</button>
+      </div>
+      <div className="beginRight">
+        <img src="/eevee.png" alt="eevee"></img>
+      </div>
     </div>
   );
 };
