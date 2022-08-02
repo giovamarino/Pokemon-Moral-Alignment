b93295416bc50183be238f3339c6ac73648f4d69
Giovanni Marino <GiovaMarino1@gmail.com>
2022-08-01 23:49:46 -0400

    add next question logic

--- src/Question.js
+++ src/Question.js
@@ -1,12 +1,28 @@
 import data from "./data/data.json";
+import { useState } from "react";
 
 const Question = () => {
+  let i = 0;
+  let [question, setQuestion] = useState(data[i].question);
+
+  let increment = () => {
+    i++;
+  };
+
   return (
     <div className="Question">
-      <p>{data[0].question}</p>
+      <p>{question}</p>
       <div>
-        <button>Yes</button>
-        <button>No</button>
+        <button
+          onClick={() => {
+            increment();
+            // console.log(i, data[i].question);
+            setQuestion(data[i].question);
+          }}
+        >
+          Yes
+        </button>
+        <button onClick={() => {}}>No</button>
       </div>
     </div>
   );
