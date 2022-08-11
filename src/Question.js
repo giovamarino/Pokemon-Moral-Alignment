77fbc9181160e12ad7b348f91000af165f743775
Giovanni Marino <GiovaMarino1@gmail.com>
2022-08-10 23:32:21 -0400

    yes and no buttons are functional

--- src/Question.js
+++ src/Question.js
@@ -9,20 +9,28 @@ const Question = ({ showResults, closeQuestion }) => {
   let incrementI = () => setI(i + 1);
 
   // decide to show question or results
-  let showQuestionOrResults = () => {
+  let showQuestionOrResults = (posOrNeg) => {
     // if there are more questions
     if (i < data.length - 1) {
       incrementI();
-      if (data[i].vertical === 0) {
-        stats[0].vertical.push(i);
+      if (data[i].vertical === 0 && data[i].reverse) {
+        stats[0].vertical.push(posOrNeg * -1);
+        console.log(stats);
+      } else if (data[i].vertical === 0) {
+        stats[0].vertical.push(posOrNeg);
         console.log(stats);
       }
-      if (data[i].horizontal === 0) {
-        stats[0].horizontal.push(i);
+      if (data[i].horizontal === 0 && data[i].reverse) {
+        stats[0].horizontal.push(posOrNeg * -1);
+        console.log(stats);
+      } else if (data[i].horizontal === 0) {
+        stats[0].horizontal.push(posOrNeg);
         console.log(stats);
       }
-    } else if (i === data.length - 1 && data[i].horizontal === 0) {
-      stats[0].horizontal.push(i);
+    }
+    // if no questions
+    if (i === data.length - 1 && data[i].vertical === 0) {
+      stats[0].vertical.push(posOrNeg);
       console.log(stats);
       closeQuestion();
       showResults();
@@ -33,8 +41,20 @@ const Question = ({ showResults, closeQuestion }) => {
     <div className="Question">
       <p>{data[i].question}</p>
       <div>
-        <button onClick={showQuestionOrResults}>Yes</button>
-        <button onClick={showQuestionOrResults}>No</button>
+        <button
+          onClick={() => {
+            showQuestionOrResults(1);
+          }}
+        >
+          Yes
+        </button>
+        <button
+          onClick={() => {
+            showQuestionOrResults(-1);
+          }}
+        >
+          No
+        </button>
       </div>
     </div>
   );
